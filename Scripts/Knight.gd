806b2aacf3300355b0e7b2a74f48d40639a7df5d
122314141a-max <122314141a@gmail.com>
2026-05-31 14:30:56 -0400

    fix it

--- Scripts/Knight.gd
+++ Scripts/Knight.gd
@@ -13,42 +13,42 @@ func Player_Movement(_delta):
 	if Input.is_action_pressed("Move_right"):
 		velocity.x = SPEED
 		amimated_sprite.play("run")
-
+		$HandActor/HitArea2D/Hitarea.disabled = true
 
 	elif Input.is_action_pressed("Move_left"):
 		velocity.x = -SPEED
 		amimated_sprite.play("run")
-
+		$HandActor/HitArea2D/Hitarea.disabled = true
 
 	elif Input.is_action_pressed("Move_down"):
 		velocity.y = SPEED
 		amimated_sprite.play("run")
-
+		$HandActor/HitArea2D/Hitarea.disabled = true
 
 	elif Input.is_action_pressed("Move_up"):
 		velocity.y = -SPEED
 		amimated_sprite.play("run")
-	
+		$HandActor/HitArea2D/Hitarea.disabled = true
 
 	elif Input.is_action_pressed("Attack"):
 		amimated_sprite.play("Attack")
-
+		$HandActor/HitArea2D/Hitarea.disabled = false
 	
 
 	else:
 			amimated_sprite.play("idle")
-
+			$HandActor/HitArea2D/Hitarea.disabled = true
 
 	move_and_slide()
 
 	
 func _ready():
-	var hitbox = get_node_or_null("AnimatedSprite2D/HitArea2D/Hitarea")
+	var hitbox = get_node_or_null("$HandActor/HitArea2D/Hitarea")
 
-	# if hitbox:
-		# $AnimatedSprite2D/HitArea2D/Hitarea.disabled = false
-	# else:
-		# print("Hitbox not found!")
-	# for child in get_children():
-		# print(child.name)
+	if hitbox:
+		$HandActor/HitArea2D/Hitarea.disabled = false
+	else:
+		print("Hitbox not found!")
+	for child in get_children():
+		print(child.name)
 	
